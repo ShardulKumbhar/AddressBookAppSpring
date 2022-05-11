12a250bdfe152dc03311311f163117fb9eb790c3
Shardulkumbhar <kumbharshardul384@gmail.com>
2022-05-11 19:27:03 +0530

    USer SL4j and Lombook

--- src/main/java/com/bridgelabz/addressbookapp/model/AddressbookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressbookData.java
@@ -1,12 +1,19 @@
 package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Getter
-@Setter
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Data
+@Entity
+@NoArgsConstructor
 public class AddressbookData {
+    @Id
     private int personId;
     private String name;
     private String phNumber;
--- src/main/java/com/bridgelabz/addressbookapp/service/AddressBookServiceImpl.java
+++ src/main/java/com/bridgelabz/addressbookapp/service/AddressBookServiceImpl.java
@@ -2,37 +2,41 @@ package com.bridgelabz.addressbookapp.service;
 
 import com.bridgelabz.addressbookapp.dto.AddressbookDTO;
 import com.bridgelabz.addressbookapp.model.AddressbookData;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class AddressBookServiceImpl implements IAddressBookService {
     List<AddressbookData> addressbookDataList = new ArrayList<>();
 
     @Override
     public List<AddressbookData> getAddressbookData() {
+        log.info("Getting All List Of AddressBook Data");
         return addressbookDataList;
     }
 
     @Override
     public AddressbookData getAddressbookDataById(int personId) {
-        AddressbookData addressbookData = null;
-        addressbookData = addressbookDataList.get(personId - 1);
+        log.info("Getting address book by ID");
+        AddressbookData addressbookData =  addressbookDataList.get(personId - 1);
         return addressbookData;
     }
 
     @Override
     public AddressbookData createAddressbooData(AddressbookDTO addressbookDTO) {
-        AddressbookData addressbookData = null;
-        addressbookData = new AddressbookData(addressbookDataList.size() + 1, addressbookDTO);
+        log.info("Created a new address book data");
+        AddressbookData addressbookData = new AddressbookData(addressbookDataList.size() + 1, addressbookDTO);
         addressbookDataList.add(addressbookData);
         return addressbookData;
     }
 
     @Override
     public AddressbookData updateAddressbookData(int personId, AddressbookDTO addressbookDTO) {
+        log.info("Updating/Editing The AddressBook Data of Given ID");
         AddressbookData addressbookData = this.getAddressbookDataById(personId);
         addressbookData.setName(addressbookDTO.name);
         addressbookData.setPhNumber(addressbookDTO.phNumber);
@@ -42,6 +46,7 @@ public class AddressBookServiceImpl implements IAddressBookService {
 
     @Override
     public void deleteAddressbooData(int personId) {
+        log.info("Deleting The AddressBook Data of Given ID");
         int i = 1;
         addressbookDataList.remove(personId - 1);
         for (AddressbookData addressbookData : addressbookDataList) {
